8bb85d92e56dc8b35e0f0eedbfd1159af0ab605e
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-05-03 23:50:28 +0300

    add CartInfo Model class

--- src/main/java/by/yemelyanenko/marketplace/model/CartInfo.java
+++ src/main/java/by/yemelyanenko/marketplace/model/CartInfo.java
@@ -1,6 +1,39 @@
 package by.yemelyanenko.marketplace.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartInfo {
 
+    private List<CartItemInfo> items = new ArrayList<>();
 
+    public List<CartItemInfo> getItems() {
+        return items;
+    }
+
+    public void addItem(CartItemInfo item) {
+        for (CartItemInfo existing : items) {
+            if (existing.getProductId().equals(item.getProductId())) {
+                existing.setQuantity(item.getQuantity() + item.getQuantity());
+            }
+            return;
+        }
+        items.add(item);
+    }
+
+    public void removeItem(Long itemId) {
+        items.removeIf(item -> item.getProductId().equals(itemId));
+    }
+
+    public int getCount() {
+        return items.stream().mapToInt(CartItemInfo::getQuantity).sum();
+    }
+
+    public double getTotal(){
+        return items.stream().mapToDouble(CartItemInfo::getTotalPrice).sum();
+    }
+
+    public void clear(){
+        items.clear();
+    }
 }
